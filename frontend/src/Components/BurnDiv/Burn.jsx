69e903fedae06b6524a64a4c2503ea8e0d7246a7
Oyeins-GUI <oyeinx7@gmail.com>
2024-04-04 01:17:22 +0100

    pulled in data for table, arranged navbar, fixed navbar links

--- frontend/src/Components/BurnDiv/Burn.jsx
+++ frontend/src/Components/BurnDiv/Burn.jsx
@@ -1,17 +1,18 @@
-import React from "react"
 import { FaQuestion } from "react-icons/fa"
-import questionImg from "../../Assets/Images/question.png"
+import questionImg from "../../assets/Images/question.png"
 
 const Burn = () => {
    return (
-      <div className="burnDiv">
-         <div className="centeredText">
-            <h2 className="text-styling">
-               <FaQuestion />
-            </h2>
-         </div>
-         <div className="questionMark">
-            <img src={questionImg} alt="Question Mark" />
+      <div className="content">
+         <div className="burnDiv">
+            <div className="centeredText">
+               <h2 className="text-styling">
+                  <FaQuestion />
+               </h2>
+            </div>
+            <div className="questionMark">
+               <img src={questionImg} alt="Question Mark" />
+            </div>
          </div>
       </div>
    )
