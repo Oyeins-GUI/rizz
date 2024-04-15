481b052f97a42b7a96218e22a5917cc5ed2c6436
Oyeins-GUI <oyeinx7@gmail.com>
2024-04-15 02:11:15 +0100

    fix incorrect file paths

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import Burn from "./components/BurnDiv/Burn.jsx"
 import NavBar from "./components/NavBar/NavBar.jsx"
-import { RizzDataProvider } from "./context.jsx"
+import { RizzDataProvider } from "./rizzDataContext.jsx"
 
 const queryClient = new QueryClient()
 
